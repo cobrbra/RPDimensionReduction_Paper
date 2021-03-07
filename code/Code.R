f09a86a338c6cc84a208043d0f0b2198cc0dbf66
Jacob Bradley <cobrbradley@gmail.com>
2021-03-07 17:43:39 +0000

    Added SIR and SAVE comparisons

--- code/Code.R
+++ code/Code.R
@@ -13,6 +13,9 @@ library(latex2exp)
 # install.packages("tidyverse")
 library(tidyverse)
 
+# install.packages("dr")
+library(dr)
+
 load_all("../../RPEnsemble/")
 
 
@@ -66,15 +69,16 @@ set.seed(1234)
 
 p <- 100
 
-n_train <- 500
-n_val <- 500
+n_train <- 800
+n_val <- 200
 n_test <- 500
 
 
 # Train/test split
 ids <- tvt(n_train, n_val, n_test)
 
-# Simulation model 1
+## Simulation model 1
+# Simulate data
 simulated_data_1 <- RPModel(1, n = n_train + n_val + n_test, p = p)
 
 XTrain_1 <- simulated_data_1$data$x[ids$train, ]
@@ -86,23 +90,44 @@ YVal_1 <- simulated_data_1$data$y[ids$val]
 XTest_1 <- simulated_data_1$data$x[ids$test, ]
 YTest_1 <- simulated_data_1$data$y[ids$test]
 
-
+# RPEnsemble Method
 decompose_1 <- RPDecomposeA(XTrain = XTrain_1, YTrain = YTrain_1, XVal = XVal_1, YVal = YVal_1, B1 = 1000, B2 = 10, d = 3, base = "knn", estmethod = "samplesplit")
 reduce_1 <- RPReduce(XTrain = XTrain_1, YTrain = YTrain_1, reduced_dim = 2, XTest = XTest_1, YTest = YTest_1, d = 2, decomposition = decompose_1) %>% 
   mutate(Model = 1)
 
+# SIR Method
+train_val_data_1 <- data.frame(cbind(rbind(XTrain_1, XVal_1), Y = c(YTrain_1, YVal_1)))
+sir_1 <- dr(data = train_val_data_1, formula = as.formula(paste("Y ~ ", paste0("V", 1:100, collapse = "+"))), method = "sir", nslices = 2)
 
+# SAVE Method
+save_1 <- dr(data = train_val_data_1, formula = as.formula(paste("Y ~ ", paste0("V", 1:100, collapse = "+"))), method = "save", nslices = 2)
+
+# Plot eigenvalue spectrum
 sim1_ev <- data.frame(sd = 1:length(decompose_1$eigv), eigv = decompose_1$eigv) %>% 
   ggplot(aes(x = sd, y = eigv)) + geom_point() + 
   theme_minimal() + labs(x = "Subspace Dimension", y = "Eigenvalue") +
   theme(axis.title.y = element_text(vjust = 2), axis.title.x = element_text(vjust = -2))
 
+# Plot two-dimensional test set representations
 sim1_rv <- reduce_1 %>% 
   ggplot(aes(x = Dim_1, y = Dim_2, colour = YTest)) + geom_point() + 
   labs(x = TeX("$\\hat{r}_1"), y = TeX("$\\hat{r}_2")) + 
   theme_minimal() + theme(legend.position = "none")
 
-# Simulation model 2
+# Get false discovery and power statistics
+rp_est_proj_1 <- decompose_1$v[, 1:2] %*% t(decompose_1$v[, 1:2])
+sir_est_proj_1 <- sir_1$evectors[, 1:2] %*% t(sir_1$evectors[, 1:2])
+save_est_proj_1 <- save_1$evectors[, 1:2] %*% t(save_1$evectors[, 1:2])
+true_proj_1 <- simulated_data_1$subspace %*% t(simulated_data_1$subspace)
+rp_fd_1 <- sum(diag((diag(100) -  true_proj_1) %*% rp_est_proj_1))
+rp_pw_1 <- sum(diag(true_proj_1 %*% rp_est_proj_1))
+sir_fd_1 <- sum(diag((diag(100) - true_proj_1) %*% sir_est_proj_1))
+sir_pw_1 <- sum(diag(true_proj_1 %*% sir_est_proj_1))
+save_fd_1 <- sum(diag((diag(100) - true_proj_1) %*% save_est_proj_1))
+save_pw_1 <- sum(diag(true_proj_1 %*% save_est_proj_1))
+
+## Simulation model 2
+# Simulate data
 simulated_data_2 <- RPModel(2, n = n_train + n_val + n_test, p = p)
 
 XTrain_2 <- simulated_data_2$data$x[ids$train, ]
@@ -114,26 +139,45 @@ YVal_2 <- simulated_data_2$data$y[ids$val]
 XTest_2 <- simulated_data_2$data$x[ids$test, ]
 YTest_2 <- simulated_data_2$data$y[ids$test]
 
-
+# RPEnsemble Method
 decompose_2 <- RPDecomposeA(XTrain = XTrain_2, YTrain = YTrain_2, XVal = XVal_2, YVal = YVal_2, B1 = 1000, B2 = 10, d = 3, base = "knn", estmethod = "samplesplit")
 reduce_2 <- RPReduce(XTrain = XTrain_2, YTrain = YTrain_2, reduced_dim = 2, XTest = XTest_2, YTest = YTest_2, d = 2, decomposition = decompose_2) %>% 
   mutate(Model = 2)
 
+# SIR Method
+train_val_data_2 <- data.frame(cbind(rbind(XTrain_2, XVal_2), Y = c(YTrain_2, YVal_2)))
+sir_2 <- dr(data = train_val_data_2, formula = as.formula(paste("Y ~ ", paste0("V", 1:100, collapse = "+"))), method = "sir", nslices = 2)
 
+# SAVE Method
+save_2 <- dr(data = train_val_data_2, formula = as.formula(paste("Y ~ ", paste0("V", 1:100, collapse = "+"))), method = "save", nslices = 2)
+
+# Plot eigenvalue spectrum
 sim2_ev <- data.frame(sd = 1:length(decompose_2$eigv), eigv = decompose_2$eigv) %>% 
   ggplot(aes(x = sd, y = eigv)) + geom_point() + 
   theme_minimal() + labs(x = "Subspace Dimension", y = "Eigenvalue") +
   theme(axis.title.y = element_text(vjust = 2), axis.title.x = element_text(vjust = -2))
 
+# Plot two-dimensional test set representations
 sim2_rv <- reduce_2 %>% 
   ggplot(aes(x = Dim_1, y = Dim_2, colour = YTest)) + geom_point() + 
   labs(x = TeX("$\\hat{r}_1"), y = TeX("$\\hat{r}_2")) + 
   theme_minimal() + theme(legend.position = "none")
 
+# Get false discovery and power statistics
+rp_est_proj_2 <- decompose_2$v[, 1:3] %*% t(decompose_2$v[, 1:3])
+sir_est_proj_2 <- sir_2$evectors[, 1:3] %*% t(sir_2$evectors[, 1:3])
+save_est_proj_2 <- save_2$evectors[, 1:3] %*% t(save_2$evectors[, 1:3])
+true_proj_2 <- simulated_data_2$subspace %*% t(simulated_data_2$subspace)
+rp_fd_2 <- sum(diag((diag(100) -  true_proj_2) %*% rp_est_proj_2))
+rp_pw_2 <- sum(diag(true_proj_2 %*% rp_est_proj_2))
+sir_fd_2 <- sum(diag((diag(100) - true_proj_2) %*% sir_est_proj_2))
+sir_pw_2 <- sum(diag(true_proj_2 %*% sir_est_proj_2))
+save_fd_2 <- sum(diag((diag(100) - true_proj_2) %*% save_est_proj_2))
+save_pw_2 <- sum(diag(true_proj_2 %*% save_est_proj_2))
 
 sim_evs <- plot_grid(sim1_ev, sim2_ev, nrow = 2, labels = "AUTO")
-ggsave(filename = "results/figures/sim_evs.png", plot = sim_evs, width = 8, height = 12)
+ggsave(filename = "results/figures/sim_evs.png", plot = sim_evs, width = 8, height = 6)
 
 write_tsv(x = bind_rows(reduce_1, reduce_2), file = "results/sim_reductions.tsv")
-sim_rvs <- plot_grid(sim1_rv, sim2_rv, nrow = 2, labels = "AUTO")
-ggsave(filename = "results/figures/sim_rvs.png", plot = sim_rvs, width = 8, height = 8)
+sim_rvs <- plot_grid(sim1_rv, sim2_rv, nrow = 1, labels = "AUTO")
+ggsave(filename = "results/figures/sim_rvs.png", plot = sim_rvs, width = 8, height = 4)
